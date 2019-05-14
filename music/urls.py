20eba92ebfd16ed24cd588902d326076a0df667d
optimusprime9017 <optimus5289@gmail.com>
2019-05-14 23:46:34 +0600

    adding simple Form

--- music/urls.py
+++ music/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #/music/
     path('',views.index,name = 'index'),
-    # music/<album_id>
+    #music/<album_id>
     path('<int:album_id>/', views.details, name='details'),
+
+#music/<album_id>/favorite
+    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
-from django.http import Http404
+#from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
-from .models import Album
-from django.shortcuts import render
+from .models import Album,Song
+from django.shortcuts import render,get_object_or_404
 
 def index(request):
 
@@ -18,8 +18,28 @@ def index(request):
 
 def details(request,album_id):
     #return HttpResponse("<h2>Details for album id: "+ str(album_id) + "</h2>")
-    try:
-        album = Album.objects.get(pk = album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album Does Not Exist")
+   #replace try
+   # try:
+    #    album = Album.objects.get(pk = album_id)
+    #except Album.DoesNotExist:
+
+     #   raise Http404("Album Does Not Exist")
+
+    album = get_object_or_404(Album,pk = album_id)
     return render(request,"music/detail.html",{'album':album})
+
+
+def favorite(request,album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song=album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, "music/detail.html", {
+            'album': album,
+            'error_message':"you did not seclect a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, "music/detail.html", {'album': album})
+
